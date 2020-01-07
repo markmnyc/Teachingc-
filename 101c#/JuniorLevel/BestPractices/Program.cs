76bc16aca3115ed89e425fb07bd23bc9fd54d8a3
Mark <markmnyc27@gmail.com>
2020-01-07 07:10:21 +0200

    More commits to sort out the structure after issues with merging

---
+++ 101c#/JuniorLevel/BestPractices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
