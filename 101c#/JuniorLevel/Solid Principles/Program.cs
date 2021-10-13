dbfa3c8eed1476efa6ff367129da18c606bc3717
Mark Miller <espada.za@live.com>
2021-10-13 20:12:29 +0200

    Added in new solid principles into the repo for learning

---
+++ 101c#/JuniorLevel/Solid Principles/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Solid_Principles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We want to achieve something simple that can be made very complex 
+            //ordering a coffee 
+            Console.WriteLine("Learning SOLID principles and ordering a coffee");
+
+
+
+
+
+            
+
+        }
+    }
+}
